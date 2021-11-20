022d35bc4836d5f05113cabdf2926f008e745c6e
Matthew <phillips.matthew829@gmail.com>
2021-11-20 14:07:02 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -28,6 +28,15 @@ function imgLoad(){
     });
 }
 
+
+//data.message is the original Object that many definitions originate from.
+//breedList is an Array.
+//breed is a String.
+//breedColor is the defining of the fontcolor of breedTag, after that String is clicked (inside Event Listener).
+//breedTag is the definition of a new <li> tag being created, along with it's .id and .innerText.
+
+let breedList = [];
+
 function breedLoad(){
     const breedUrl = 'https://dog.ceo/api/breeds/list/all';
     fetch(breedUrl, {
@@ -38,21 +47,23 @@ function breedLoad(){
         console.log(data.message); //For getting the needed info for the future steps
         const dogBreedContainer = document.getElementById('dog-breeds'); // get destination for dog breeds
     for(breed in data.message){ //loop over data.message and for each string:
-        let breedTag = document.createElement('li'); //Create new p tag
+        console.log(breed);
+        let breedTag = document.createElement('li'); //Create new <li> tag
         breedTag.id = breed;
         breedTag.addEventListener("click", () => { //Add Event Listener to the <li> tag
             // console.log("breed ", breedTag);
-            let breedColor = breedTag.innerText.fontcolor("red");
+            let breedColor = breedTag.innerText.fontcolor("red"); //Turn the text "red"
             breedTag.innerHTML = breedColor;
         });
         let breedDropDown = document.getElementById('breed-dropdown');
         breedDropDown.addEventListener("onchange", () => {
-            let dogFilter = breedTag.filter(breedTag => {
-                .charAt(0) === breedTag.innerText.charAt(0);
-            })
+            let dogFilter = breedList.filter(breedTag => { //Creating the filtered result...
+        //         /**/ === breedTag.innerText.charAt(0);
+    })
         });
         breedTag.innerText = breed;
-        dogBreedContainer.appendChild(breedTag); //Append corresponding breed element to dog breed destination
+        dogBreedContainer.appendChild(breedTag); //Append corresponding <li> breed element to dogBreedContainer
+        breedList.push(breed); //Push breed into breedList
         }
     })
     .catch(function (error){
